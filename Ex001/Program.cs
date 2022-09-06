824476430848d9c6df8e8a138f620fc39b8b4261
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-06 20:12:28 +0300

    программа принимает на вход вда числа и выводит какое из них максимальное , а каое минимальное

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -3,4 +3,26 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
+Console.Clear();
+try
+{
+Console.WriteLine ("Введите число A:");
+int numberA = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число B:");
+int numberB = Convert.ToInt32 (Console.ReadLine ());
 
+if (numberA == numberB) Console.WriteLine ("Числа равны");
+
+else
+    { 
+        if (numberA > numberB )
+            Console.WriteLine ("A = " + numberA + "; " + "B = " + numberB + " -> max = " + numberA + " -> min = " + numberB);
+        else 
+            Console.WriteLine ("A = " + numberA + "; " + "B = " + numberB + " -> max = " + numberB + " -> min = " + numberA);
+    }
+}
+
+catch
+{
+    Console.WriteLine ("Вы ввели не целое число");
+}
