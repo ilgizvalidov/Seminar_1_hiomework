2871008e9369b457847eb2d4c352e0e8170f4adc
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-08 09:29:29 +0300

    Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -2,3 +2,30 @@
 // 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.Clear ();
+try 
+{
+    Console.WriteLine ("Введите число");
+    int number = Convert.ToInt32 (Console.ReadLine());
+    int curent = (number / 2); // считаем сколько раз надо выполнить цикл до достижения number с шагом 2 т,к выводиться будут четные цифры начиная с 2
+    int result = 2;
+    int i = 1; // счетчик
+    if (number > 2 )
+    {      
+     while ( i < curent)
+        {
+            Console.Write(result + ",");
+            result = (result + 2); 
+            i++;
+        }
+    Console.Write (result); // выводим последнюю цифру без запятой 
+    }
+    else 
+        Console.WriteLine ("введите целое число больше 2"); // проверка вводимого числа на нижний порог значения
+ }
+
+catch 
+{
+    Console.WriteLine ("Введите целое число"); // общее сообщение, если пользователь вчто-то кроме целого числа
+}
